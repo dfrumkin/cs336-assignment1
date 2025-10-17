515474050d5e89c15f85a5f1f4dc2aefe4c9b6be
Dmitry Frumkin <dmitry.frumkin@gmail.com>
2025-10-17 22:28:43 +0300

    Linear

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,6 +9,7 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
+from cs336_basics.linear import Linear
 from cs336_basics.tokenizer import Tokenizer
 from cs336_basics.train_bpe import train_bpe
 
@@ -31,8 +32,9 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
-
-    raise NotImplementedError
+    linear = Linear(d_in, d_out)
+    linear.load_state_dict({"weights": weights})
+    return linear(in_features)
 
 
 def run_embedding(
